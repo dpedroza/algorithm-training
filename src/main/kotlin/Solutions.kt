eb63b15f0dc82b639ae145d096c8caf4ef76981d
dpedroza <david.pedroza@loggi.com>
2020-04-15 21:40:45 -0300

    add sell/buy stock challenge and tests

--- src/main/kotlin/Solutions.kt
+++ src/main/kotlin/Solutions.kt
@@ -1,4 +1,6 @@
+fun main() {
 
+}
 /**
  * Solutions for the 30 day
  * #stayhome leetcode challenge
@@ -59,5 +61,28 @@ object Solutions {
             mainValue /= 10
         }
         return isHappy(newValue)
+    }
+
+    /**
+     * Challenge Buy/Sell stock II
+     *
+     * @param prices prices of a stock for given index
+     * @return maxProfit of these combinations
+     */
+    fun maxProfit(prices: IntArray): Int {
+
+        var profit = 0
+
+        for (i in 1 until prices.size) {
+            val difference = prices[i] - prices[i - 1]
+
+            if (difference > 0) {
+                profit += difference
+            }
+        }
+
+        println(profit)
+
+        return profit
     }
 }
---
+++ src/test/kotlin/MaxProfitTest.kt
@@ -0,0 +1,34 @@
+import Solutions.maxProfit
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class MaxProfitTest(
+    private val input: IntArray,
+    private val expected: Int
+) {
+
+    private companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "test case: {index} ==> max profit is {1}")
+        fun parameters() = listOf(
+            arrayOf(
+                intArrayOf(7, 1, 5, 3, 6, 4), 7
+            ),
+            arrayOf(
+                intArrayOf(1, 2, 3, 4 ,5), 4
+            ),
+            arrayOf(
+                intArrayOf(7, 6, 4, 3, 1), 0
+            )
+        )
+    }
+
+    @Test
+    fun singleNumberAssertions() {
+        assertThat(maxProfit(input), equalTo(expected))
+    }
+}
