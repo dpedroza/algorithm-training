e15980059b8dcec8f742db3a095af3bb32566064
David Pedroza <davidpedroza@gmail.com>
2020-06-01 22:12:52 -0300

    split python and kotlin code

--- python/sorted_squares.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        result = []
-        for idx, n in enumerate(A):
-            positive_number =  n * -1
-            square = positive_number * positive_number
-            result.append(square)
-        return sorted(result)   
